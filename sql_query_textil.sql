b4deb95743ee1508285545e264c11b4f246bb4bf
nicodiaz <ngdiaz00@gmailcom>
2025-03-08 18:49:35 -0300

    Se agregan nuevas tablas al sql

--- sql_query_textil.sql
+++ sql_query_textil.sql
@@ -1,6 +1,7 @@
 use master;
+
 go
-use GESTOR_TEXTIL_DB;
+use TallerTextil_DB;
 go
 --create table CLIENTES (
 --Id int identity(1,1) not null,
@@ -19,35 +20,72 @@ go
 --Archivo varbinary(max) not null,
 --constraint PK_ID_IMAGEN primary key(Id)
 --)
-create table TRABAJOS(
-Id int identity(1,1) not null,
-Fecha date not null,
-Id_estado int not null,
-Id_Tipo int not null,
-Valor decimal(10,2) not null,
-constraint PK_ID_TRABAJO primary key(Id)
-)
+--create table TRABAJOS(
+	--Id int identity(1,1) not null,
+	--Fecha date not null,
+	--Id_estado int not null,
+	--Id_Tipo int not null,
+	--Valor decimal(10,2) not null,
+	--constraint PK_ID_TRABAJO primary key(Id)
+--)
 create table ESTADOS (
-Id int identity(1,1) not null,
-Nombre varchar (20) not null,
-constraint PK_ID_ESTADO primary key(Id)
+	Id int identity(1,1) not null,
+	Nombre varchar (20) not null,
+	constraint PK_ID_ESTADO primary key(Id)
 )
+
+create table IMAGENES (
+	Id int identity (1,1) not null,
+	Nombre varchar(50) not null,
+	Archivo VARBINARY (MAX) not null,
+	constraint PK_ID_IMAGEN primary key (Id)
+)
+create table MATRICES (
+	Id int identity (1,1) not null,
+	Nombre varchar (50) not null,
+	archivo_dst VARBINARY(MAX) not null,
+	Id_imagen int not null,
+	constraint PK_ID_MATRIZ primary key(Id),
+	constraint FK_ID_IMAGEN foreign key (Id_imagen) REFERENCES IMAGENES (Id)
+	)
 create table BORDADOS (
-Id int identity (1,1) not null,
-Posicion varchar(20) not null,
-Id_Matriz int not null,
-Valor decimal (10,2) not null,
-constraint PK_ID_BORDADO primary key (Id)
+	Id int identity (1,1) not null,
+	Posicion varchar(20) not null,
+	Id_Matriz int not null,
+	Valor decimal (10,2) not null,
+	constraint PK_ID_BORDADO primary key (Id),
+	constraint FK_ID_MATRIZ foreign key (Id_Matriz) references	MATRICES (Id)
 )
 create table CONFECCIONES (
-Id int identity (1,1) not null,
-Valor decimal (10,2) not null,
-constraint PK_ID_CONFECCION primary key(Id)
+	Id int identity (1,1) not null,
+	Valor decimal (10,2) not null,
+	constraint PK_ID_CONFECCION primary key(Id)
 )
 create table TELAS (
-Id int identity(1,1) not null,
-Nombre varchar (20) not null,
-Color varchar (20) not null,
-constraint PK_ID_TELA primary key (Id)
+	Id int identity(1,1) not null,
+	Nombre varchar (20) not null,
+	Color varchar (20) not null,
+	constraint PK_ID_TELA primary key (Id)
 )
+
+
+create table PRENDAS (
+	Id int identity(1,1) not null,
+	Id_tipo int not null,
+	Id_tela int not null,
+	Id_talle int not null,
+	Valor decimal(10,2) not null,
+	Descripcion varchar(100),
+	constraint PK_ID_PRENDA primary key(Id),
+	constraint FK_ID_TELA foreign key (Id_tela) REFERENCES TELAS (Id)
+)
+CREATE TABLE CONFECCION_PRENDA (
+    Id INT IDENTITY (1,1) NOT NULL,
+    Id_confeccion INT NOT NULL,
+    Id_prenda INT NOT NULL,
+    CONSTRAINT PK_CONFECCION_PRENDA PRIMARY KEY (Id),
+    CONSTRAINT FK_ID_CONFECCION FOREIGN KEY (Id_confeccion) REFERENCES CONFECCIONES (Id),
+    CONSTRAINT FK_ID_PRENDA FOREIGN KEY (Id_prenda) REFERENCES PRENDAS (Id)
+);
+
 
